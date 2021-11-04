7318530aec3fa5165c6db0114d77de35558b561b
Nathan Heidacker <45582949+nathanheidacker@users.noreply.github.com>
2021-11-04 18:39:43 -0500

    Add files via upload

---
+++ Data Cleaning Project/the-magic-squids/src/sql.sql
@@ -0,0 +1,6 @@
+SELECT * FROM trr_trr_refresh;
+SELECT * FROM trr_actionresponse_refresh;
+SELECT * FROM trr_charge_refresh;
+SELECT * FROM trr_subjectweapon_refresh;
+SELECT * FROM trr_trrstatus_refresh;
+SELECT * FROM trr_weapondischarge_refresh;
